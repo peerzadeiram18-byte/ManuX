d32f5d870e10a7d35bf7feb9171a76c8beb9a5d4
Iram Peerzade <peerzadeiram18@gmail.com>
2026-03-27 00:18:48 +0530

    updated data

--- src/pages/ColorCosmetics.jsx
+++ src/pages/ColorCosmetics.jsx
@@ -1,9 +1,13 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ProductContext } from "../context/ProductContext";
 import "./ColorCosmetics.css";
 
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
 export default function ColorCosmetics() {
   const { products } = useContext(ProductContext);
+
+  const [selectedImage, setSelectedImage] = useState(null);
 
   // sirf color cosmetics products
   const filtered = products.filter(
@@ -18,12 +22,32 @@ export default function ColorCosmetics() {
       <div className="cc-grid">
         {filtered.map((item) => (
           <div className="cc-card" key={item._id}>
-            <img src={item.image} alt={item.name} />
+
+            <img
+              src={`${BASE_URL}/uploads/${item.image}`}
+              alt={item.name}
+              onClick={() =>
+                setSelectedImage(`${BASE_URL}/uploads/${item.image}`)
+              }
+              onError={(e) => (e.target.src = "/no-image.png")}
+            />
+
             <h3>{item.name}</h3>
             <p>{item.description}</p>
+
           </div>
         ))}
       </div>
+
+      {/* IMAGE MODAL */}
+      {selectedImage && (
+        <div
+          className="cc-modal"
+          onClick={() => setSelectedImage(null)}
+        >
+          <img src={selectedImage} alt="preview" />
+        </div>
+      )}
 
     </div>
   );
